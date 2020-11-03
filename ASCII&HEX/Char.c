51c91b838f4da7e8b6837f12804451e3f01e5505
Sir. ObF <63687784+Plattyz@users.noreply.github.com>
2020-11-03 10:18:38 -0500

    Create Char.c

---
+++ ASCII&HEX/Char.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char fichier[1000000000];
+int main()
+{
+  FILE *fpt, *fpt2;
+    printf("Please enter the name of the file you want to convert: \n");
+       fgets(fichier, sizeof(fichier), stdin);
+  fichier[strlen(fichier)-1] = 0x00;
+      fpt = fopen(fichier, "rb");
+    fpt2 = fopen("char.txt", "w");
+char copy;
+   int i;
+   while( ( copy = fgetc(fpt) ) != EOF )
+   {
+      fprintf(fpt2, "%c ", copy);
+      if( !(++i % 16) ) ;
+   }
+   fclose(fpt);
+   fclose(fpt2);
+   putc('\n', stdout);
+
+   return 0;
+}
