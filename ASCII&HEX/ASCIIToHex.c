b385577d6186c1914bda05ae81e3cea8c8b82e4d
Sir. ObF <63687784+Plattyz@users.noreply.github.com>
2020-11-02 22:26:00 -0500

    Create ASCIIToHex.c

---
+++ ASCII&HEX/ASCIIToHex.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+char fichier[1000000000];
+int main(){
+    FILE *fpt, *fpt2;
+    printf("Please enter the name of the file you want to convert: \n");
+    gets(fichier);
+    fpt = fopen(fichier, "r");
+    fpt2 = fopen("hex.txt", "w");
+char copy;
+while((copy = fgetc(fpt)) != EOF)
+fprintf(fpt2, "%02X ", copy);
+    fclose(fpt);
+    fclose(fpt2);
+return 0;
+}
